1fd7e928168bab05b3c92bf24657c880eb5a97ec
shubhamsks <shubhamkumar7051@gmail.com>
2019-04-10 14:50:42 +0530

    Added new host url

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'c8@ifx)@i$zps@!hl^@%^-+5)7h+^_%d^7b4=e+3)*+9ca92@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'codersblog.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hackersblog.pythonanywhere.com']
 
 
 # Application definition
